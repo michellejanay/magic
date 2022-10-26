7eaf83920e0c942f3cb12c0e2de00237d29e12c0
Chelle <dearmichelley@gmail.com>
2022-10-26 01:44:20 +0100

    results: updated

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const shake = document.getElementById("shake-ball");
 const shakeAgain = document.getElementById("shake-again");
 const result = document.getElementById("result");
+const img = document.getElementById("result-img");
 const headerMsg = document.getElementById("header-msg");
+let str = "";
+let alt = "";
+let source = "";
 
 shake.addEventListener("click", () => {
   const shaked = shakeBall();
+  const check = checkAnswer(shaked);
   // add transitions/setInterval
-  result.innerHTML = shaked.toLowerCase();
+  str = shaked.toLowerCase();
+  //result.innerHTML = str;
 
-  headerMsg.innerHTML = checkAnswer(shaked).toLowerCase();
+  headerMsg.innerHTML = check.toLowerCase();
   shake.style.display = "none";
-  result.style.display = "flex";
+  //result.style.display = "flex";
+  result.style.display = "none";
   shakeAgain.style.display = "block";
+  results();
+
+  img.src = source;
+  img.alt = alt;
 });
 
 shakeAgain.addEventListener("click", () => {
@@ -21,41 +32,51 @@ shakeAgain.addEventListener("click", () => {
     "think of your yes || no question and press the button!";
   shake.style.display = "block";
   shakeAgain.style.display = "none";
+  img.src = "";
+  img.alt = "";
 });
 
-// const results = () => {
-//   const memory = {
-//     iic: { src: "8ball/answers/iic.png", alt: "it is certain." },
-//     iids: { src: "8ball/answers/iids.png", alt: "it is decidely so." },
-//     wad: { src: "8ball/answers/wad.png", alt: "without a doubt." },
-//     yd: { src: "8ball/answers/yd.png", alt: "yes, definitely." },
-//     ymroi: { src: "8ball/answers/ymroi.png", alt: "you may rely on it." },
-//     aisiy: { src: "8ball/answers/aisiy.png", alt: "as i see it, yes." },
-//     ml: { src: "8ball/answers/ml.png", alt: "most likely." },
-//     og: { src: "8ball/answers/og.png", alt: "outlook good." },
-//     y: { src: "8ball/answers/y.png", alt: "yes." },
-//     spty: { src: "8ball/answers/spty.png", alt: "signs point to yes." },
-//     rhta: { src: "8ball/answers/rhta.png", alt: "reply hazy, try again." },
-//     agl: { src: "8ball/answers/agl.png", alt: "ask again later." },
-//     bntyn: { src: "8ball/answers/bntyn.png", alt: "better not tell you now." },
-//     cpn: { src: "8ball/answers/cpn.png", alt: "cannot predict now." },
-//     caaa: { src: "8ball/answers/caaa.png", alt: "concentrate and ask again." },
-//     dcoi: { src: "8ball/answers/dcoi.png", alt: "don't count on it." },
-//     myin: { src: "8ball/answers/myin.png", alt: "my reply is no." },
-//     mssn: { src: "8ball/answers/mssn.png", alt: "my sources say no." },
-//     onsg: { src: "8ball/answers/onsg.png", alt: "outlook not so good." },
-//     vd: { src: "8ball/answers/vd.png", alt: "very doubtful." },
-//   };
-//   console.log(memory.y);
-// };
-// results();
+const results = () => {
+  const memory = [
+    { src: "8ball/answers/iic.png", alt: "it is certain." },
+    { src: "8ball/answers/iids.png", alt: "it is decidely so." },
+    { src: "8ball/answers/wad.png", alt: "without a doubt." },
+    { src: "8ball/answers/yd.png", alt: "yes, definitely." },
+    { src: "8ball/answers/ymroi.png", alt: "you may rely on it." },
+    { src: "8ball/answers/aisiy.png", alt: "as i see it, yes." },
+    { src: "8ball/answers/ml.png", alt: "most likely." },
+    { src: "8ball/answers/og.png", alt: "outlook good." },
+    { src: "8ball/answers/y.png", alt: "yes." },
+    { src: "8ball/answers/spty.png", alt: "signs point to yes." },
+    { src: "8ball/answers/rhta.png", alt: "reply hazy, try again." },
+    { src: "8ball/answers/aal.png", alt: "ask again later." },
+    { src: "8ball/answers/bntyn.png", alt: "better not tell you now." },
+    { src: "8ball/answers/cpn.png", alt: "cannot predict now." },
+    { src: "8ball/answers/caaa.png", alt: "concentrate and ask again." },
+    { src: "8ball/answers/dcoi.png", alt: "don't count on it." },
+    { src: "8ball/answers/mrin.png", alt: "my reply is no." },
+    { src: "8ball/answers/mssn.png", alt: "my sources say no." },
+    { src: "8ball/answers/onsg.png", alt: "outlook not so good." },
+    { src: "8ball/answers/vd.png", alt: "very doubtful." },
+  ];
+  console.log(str);
+
+  memory.forEach((sorc) => {
+    if (sorc.alt === str) {
+      alt = sorc.alt;
+      source = sorc.src;
+    }
+  });
+  console.log(alt);
+  console.log(source);
+};
 
 function shakeBall() {
   const results = [
     "It is certain.",
     "It is decidedly so.",
     "Without a doubt.",
-    "Yes - definitely.",
+    "Yes, definitely.",
     "You may rely on it.",
     "As I see it, yes.",
     "Most likely.",
