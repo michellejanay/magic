fe10af46d75e4c5ade977c7ca9bf36b0c817e279
Chelle <dearmichelley@gmail.com>
2022-10-26 02:31:35 +0100

    script: refactored

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const shake = document.getElementById("shake-ball");
 const shakeAgain = document.getElementById("shake-again");
-const result = document.getElementById("result");
 const img = document.getElementById("result-img");
 const headerMsg = document.getElementById("header-msg");
 let str = "";
@@ -9,35 +8,31 @@ let source = "";
 
 shake.addEventListener("click", () => {
   const shaked = shakeBall();
-  const check = checkAnswer(shaked);
-  // add transitions/setInterval
   str = shaked.toLowerCase();
-  //result.innerHTML = str;
-
+  memory();
+  // add transitions/setInterval
   headerMsg.innerHTML = "the ball has shaken!";
   shake.style.display = "none";
-  //result.style.display = "flex";
-  result.style.display = "none";
   shakeAgain.style.display = "block";
-  results();
-
   img.src = source;
   img.alt = alt;
 });
 
 shakeAgain.addEventListener("click", () => {
-  //location.reload();
-  result.style.display = "none";
-  headerMsg.innerHTML = `<p>think of your</p>
-                <p id='y-n'>yes || no</p>
-                <p>question and press the button!</p>`;
+  headerMsg.innerHTML = display();
   shake.style.display = "block";
   shakeAgain.style.display = "none";
   img.src = "";
   img.alt = "";
 });
 
-const results = () => {
+const display = () => {
+  return `<p>think of your</p>
+  <p id='y-n'>yes || no</p>
+  <p>question and press the button!</p>`;
+};
+
+const memory = () => {
   const memory = [
     { src: "8ball/answers/iic.png", alt: "it is certain." },
     { src: "8ball/answers/iids.png", alt: "it is decidely so." },
@@ -68,8 +63,6 @@ const results = () => {
       source = sorc.src;
     }
   });
-  console.log(alt);
-  console.log(source);
 };
 
 function shakeBall() {
@@ -96,47 +89,5 @@ function shakeBall() {
     "Very doubtful.",
   ];
   let randomResults = Math.floor(Math.random() * results.length);
-  console.log("The ball has shaken!");
   return results[randomResults];
-}
-
-function checkAnswer(answer) {
-  const veryPositive = [
-    "It is certain.",
-    "It is decidedly so.",
-    "Without a doubt.",
-    "Yes - definitely.",
-    "You may rely on it.",
-  ];
-  const positive = [
-    "As I see it, yes.",
-    "Most likely.",
-    "Outlook good.",
-    "Yes.",
-    "Signs point to yes.",
-  ];
-  const negative = [
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not tell you now.",
-    "Cannot predict now.",
-    "Concentrate and ask again.",
-  ];
-  const veryNegative = [
-    "Don't count on it.",
-    "My reply is no.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful.",
-  ];
-
-  if (veryPositive.includes(answer)) {
-    return "very positive";
-  } else if (positive.includes(answer)) {
-    return "positive";
-  } else if (negative.includes(answer)) {
-    return "negative";
-  } else if (veryNegative.includes(answer)) {
-    return "very negative";
-  }
 }
